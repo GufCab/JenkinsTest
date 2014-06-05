09a412b5cc4684bfc8482bcb3b48b5261f3ea2b6
GufCab <nikolaj@donald.dk>
2014-06-05 21:23:45 +0200

    Testproject added

---
+++ JenkinsTestProject/JenkinsTestProject/ImplementationClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsTestProject
+{
+    public interface IImplementationClass
+    {
+        int AddTwoNumbers(int a, int b);
+    }
+
+    public class ImplementationClass : IImplementationClass
+    {
+        public int AddTwoNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ JenkinsTestProject/UnitTests/ImplementationClassTester.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using JenkinsTestProject;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class ImplementationClassTester
+    {
+        [Test]
+        public void AddFunctionShouldPass()
+        {
+            int a = 10;
+            int b = 10;
+
+            IImplementationClass uut = new ImplementationClass();
+
+            Assert.AreEqual(uut.AddTwoNumbers(a,b), 20);
+        }
+
+    }
+}
